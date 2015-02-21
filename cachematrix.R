f7103ebf31c70b296e16c955afcae3b510b5ad75
awilmsmeier <andreas.wilmsmeier@tli-usa.com>
2015-02-21 17:15:20 +0100

    Assignment 2 - First Version
    First Version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions calculate the inverse of a matrix, if not available in the cache
+## or retrieve the result of the calculation from the cache, if it has been calculated
+## previously
 
-## Write a short comment describing this function
+## Create a list containing the functions "set" and "get" to set/get the matrix associated with 
+## this instance of the list and "setinverse" / "getinverse" to set/get the inverse of the matrix
+## As per lexical scoping rules, the variables "mtrx" and "invMtrx" are become part of the
+## instance of the list created here
+makeCacheMatrix <- function(mtrx = matrix()) {
+   invMtrx <- NULL
+   
+   ## Function "set" to store the input matrix and initiate the resultimatrix 
+   set <- function(inp) {
+      mtrx <<- inp
+      invMtrx <<- NULL
+   }
 
-makeCacheMatrix <- function(x = matrix()) {
+   ## Function "get" to retrieve the input matrix 
+   get <- function() mtrx
 
+   ## Function "setmatrix" to store the inverted matrix in the cache 
+   setmatrix <- function(inv) invMtrx <<- inv
+
+   ## Function "getmatrix" to retrieve the inverted matrix in the cache 
+   getmatrix <- function() invMtrx
+   list(set = set, get = get, setmatrix = setmatrix, getmatrix = getmatrix)
 }
 
+## Return the inverse of a matrix either from the cache (if available) or 
+## by calculating it, accept the cached matrix as a parameter
+cacheSolve <- function(cacheMtrx, ...) {
 
-## Write a short comment describing this function
+   ## Try to get the inverted matrix from the cache
+   invMtrx <- cacheMtrx$getmatrix()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   ## If it is available, return the cached result 
+   if(!is.null(invMtrx)) {
+      message("getting cached data")
+
+   ## If it is not available, calculate hte inverse and store the result in the cache 
+   } else {
+      mtrx <- cacheMtrx$get()
+      invMtrx <- solve(mtrx, ...)
+      cacheMtrx$setmatrix(invMtrx)
+   }
+   invMtrx 
 }
